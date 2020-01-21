4f978739fab9c01fc6e8d99bf86f3c9d7246d150
jordanbouvier <jordanbouvierpro@gmail.com>
2020-01-21 04:55:56 +0100

    latest threads / criterias fixtures

---
+++ src/fixtures/ratingCriterias.ts
@@ -0,0 +1,52 @@
+
+export const criterias = [
+  {
+    name: "Sincerité",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Honnêteté",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Hypocrisie",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  
+  {
+    name: "Sagesse",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Sources",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Arguments",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Agressivité",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Colère",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Manipilation",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Mensonge",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Faits",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+  {
+    name: "Objectivité",
+    value:  Math.floor(Math.random() * 100) + 2
+  },
+]
