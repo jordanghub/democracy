35096906dedf63c17ddf7fe23fa944c0cc725fbe
Jordan <jordanbouvierpro@gmail.com>
2020-01-22 12:02:52 +0100

    fix flasing material style

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,21 @@
 import Document, { DocumentContext } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheets } from '@material-ui/core/styles'
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
+    const materialStyle = new ServerStyleSheets()
+    
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => {
+            const styles = sheet.collectStyles(materialStyle.collect(<App {...props} />))
+            return styles;
+          },
         })
 
       const initialProps = await Document.getInitialProps(ctx)
@@ -18,6 +24,7 @@ export default class MyDocument extends Document {
         styles: (
           <>
             {initialProps.styles}
+            {materialStyle.getStyleElement()}
             {sheet.getStyleElement()}
           </>
         ),
